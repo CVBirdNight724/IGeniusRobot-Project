4d11c0066f7a87d4d9b0f3e320d52bfe9098818d
CV2BirdNight <ro.witthawin@gmail.com>
2020-11-06 23:13:50 +0700

    Add feature in Sumo_Manual

--- TRRO2020/Sumon_Auto_ATX2/Sumon_Auto_ATX2.ino
+++ TRRO2020/Sumon_Auto_ATX2/Sumon_Auto_ATX2.ino
@@ -1,5 +1,26 @@
 #include "ATX2.h"
 
+#define FL_CHANNEL      1
+#define FR_CHANNEL      2
+#define BL_CHANNEL      4
+#define BR_CHANNEL      3 
+
+
+#define BASE_SPEED_L    70
+#define BASE_SPEED_R    70
+#define FL_MIN          0
+#define FR_MIN          0
+#define BL_MIN          0
+#define BR_MIN          0
+#define FL_MAX          100
+#define FR_MAX          100
+#define BL_MAX          100
+#define BR_MAX          100
+#define FL_REVERSE      true
+#define FR_REVERSE      true
+#define BL_REVERSE      false
+#define BR_REVERSE      false
+
 void setup() {
   // put your setup code here, to run once:
 
